66b118633781ba79b3da1d761dc5ab7bf0eec5be
Leaf developer <yeshuo20061031@outlook.com>
2022-08-10 18:20:41 +0800

    first commit

---
+++ Linux&Mac/install.sh
@@ -0,0 +1,19 @@
+clear
+echo MY-VSCODE-EXTENSION-FOR-WINDOWS
+echo BY Leaf developer
+echo 使用前请确认你的vscode已加入环境变量，可在命令行中使用".code"命令检查
+code --install-extension ms-ceintl.vscode-language-pack-zh-hans
+code --install-extension github.github-vscode-theme
+code --install-extension ritwickdey.liveserver
+code --install-extension pkief.material-icon-theme
+code --install-extension techer.open-in-browser
+code --install-extension hookyqr.minify
+code --install-extension formulahendry.auto-close-tag
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension mrcrowl.easy-less
+code --install-extension shd101wyy.markdown-preview-enhanced
+code --install-extension ecmel.vscode-html-css
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-python.python
+code --install-extension w88975.code-translate
+echo Goodbye!
